972ddd013632321ed8c8c9abd3aaa29ef8859007
wonseoke <wonseoke@gmail.com>
2020-06-11 02:30:23 -0400

    Summing the prices

--- shopping_cart.py
+++ shopping_cart.py
@@ -44,6 +44,8 @@ def to_usd(my_price):
 # Info Capture / Input
 #
 
+total_price = 0
+
 while True:
     selected_id = input("Please input a product identifier: ") #"9" (string version of input)
     if selected_id == "DONE":
@@ -51,17 +53,16 @@ while True:
     else:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
+        total_price += matching_product["price"]
         print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-
-
-
-
-
 
 
 #
 # Info Display / Output
 #
+
+
+print("TOTAL PRICE: " + str(total_price))
 
 
 #A grocery store name of your choice
