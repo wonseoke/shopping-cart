45ded5510a09eacf95191ea3a9744cb6595a034b
wonseoke <wonseoke@gmail.com>
2020-06-13 16:09:34 -0400

    Handles Invalid Input

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,5 +1,6 @@
 # shopping_cart.py
 
+from datetime import datetime
 
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
@@ -51,12 +52,14 @@ while True:
     selected_id = input("Please input a product identifier: ") #"9" (string version of input)
     if selected_id == "DONE":
         break
+    elif float(selected_id) < 21:
+        selected_ids.append(selected_id)
     else:
         #matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         #matching_product = matching_products[0]
         #total_price += matching_product["price"]
         #print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-        selected_ids.append(selected_id)
+        print("Invalid product identifier. Either input DONE or a valid product identifier")
 
 #
 # Info Display / Output
@@ -71,7 +74,7 @@ print("wskwsk.squarespace.com")
 print("8282 Fastest Road, Edgeoftomorrow, FF 12345")
 print("---------------------------------")
 #The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
-from datetime import datetime
+
 
 now = datetime.now()# datetime object containing current date and time
 dt_string = now.strftime("%Y-%m-%d %H:%M %p")
